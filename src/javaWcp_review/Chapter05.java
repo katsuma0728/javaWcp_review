2cc9de6c8715d2a83ee67bfd7350175585399c95
katsuma0728 <katsuma20220728@gmail.com>
2023-09-16 13:39:51 +0900

    [Add]Chapter05

---
+++ src/javaWcp_review/Chapter05.java
@@ -0,0 +1,15 @@
+package javaWcp_review;
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		String str = "Katsuma";
+		System.out.println(str);
+
+		str = "断固たる決意";
+		System.out.println(str);
+
+		final int luckyNumber;
+		luckyNumber = 7;
+		System.out.println(luckyNumber);
+	}
+}
---
+++ src/javaWcp_review/Variable02.java
@@ -0,0 +1,15 @@
+package javaWcp_review;
+
+public class Variable02 {
+	static final int MIN_NUMBER = 10;
+
+	public static void main(String[] args) {
+		System.out.println(MIN_NUMBER);
+		// MIN_NUMBER = 20;
+
+		final int maxValue;
+		maxValue = 100;
+		System.out.println(maxValue);
+		// maxValue = 200;	
+	}
+}
